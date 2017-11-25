9d607f4b8a86afd019ccf817287de8c9c335a927
fortyrunner <fortyrunner@gmail.com>
2017-11-25 21:31:33 +0000

    Add isolation tests

--- src/main/java/fortyrunner/Basics.java
+++ src/main/java/fortyrunner/Basics.java
@@ -1,5 +1,8 @@
 package fortyrunner;
 
+/**
+ * Bit twiddling courtesy of Hackers Delight by Henry S. Warren
+ */
 public final class Basics {
 
   private Basics(){
@@ -14,5 +17,28 @@ public final class Basics {
   public static boolean isPowerOf2(final int x){
     return turnOffRightmostBits(x) == 0;
   }
+
+  /**
+   * Is the number of the form 2^n-1?
+   */
+  public static boolean is2nMinusOne(final int x){
+    return (x & (x + 1)) == 0;
+  }
+
+  /**
+   * E.g. 0101 1000 -> 0000 1000
+   */
+  public static int isolateRightmostOneBit(final int x){
+    return x & (-x);
+  }
+
+  /**
+   * E.g. 10100111 -> 00001000
+   */
+  public static int isolateRightmostZeroBit(final int x){
+    return -x & (x + 1);
+  }
+
+
 
 }
--- src/test/java/fortyrunner/BasicsTest.java
+++ src/test/java/fortyrunner/BasicsTest.java
@@ -32,4 +32,36 @@ public class BasicsTest {
     assertTrue( Basics.isPowerOf2(0b1000000));
   }
 
+  @Test
+  public void is2nMinusOne() {
+
+    assertTrue( Basics.is2nMinusOne(0));
+    assertTrue( Basics.is2nMinusOne(0b1));
+    assertFalse( Basics.is2nMinusOne(0b10));
+    assertTrue( Basics.is2nMinusOne(0b11));
+    assertTrue( Basics.is2nMinusOne(0b111));
+    assertFalse( Basics.is2nMinusOne(0b1001));
+    assertTrue( Basics.is2nMinusOne(0b1111));
+  }
+
+  @Test
+  public void isolateRightmostOneBit() {
+    assertEquals(0b1, Basics.isolateRightmostOneBit(0b1));
+    assertEquals(0b0, Basics.isolateRightmostOneBit(0b0));
+    assertEquals(0b10, Basics.isolateRightmostOneBit(0b10));
+    assertEquals(0b01, Basics.isolateRightmostOneBit(0b11));
+    assertEquals(0b00001000, Basics.isolateRightmostOneBit(0b00001000));
+
+  }
+
+  @Test
+  public void isolateRightmostZeroBit() {
+    assertEquals(0b10, Basics.isolateRightmostZeroBit(0b01));
+    assertEquals(0b0, Basics.isolateRightmostZeroBit(0b0));
+    assertEquals(0b10, Basics.isolateRightmostZeroBit(0b10));
+    assertEquals(0b1000, Basics.isolateRightmostZeroBit(0b111));
+    assertEquals(0b0001000, Basics.isolateRightmostZeroBit(0b00001000));
+
+  }
+
 }
